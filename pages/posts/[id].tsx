c448b169564c5d6034e1cda0d6ce903e1f97f682
haenanim121@gmail.com <haenanim121@gmail.com>
2023-07-17 19:12:52 +0900

    feat : content 표시

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,7 +1,46 @@
+import { getAllPostIds, getPostData, getSortedPostsData } from '@/lib/post';
+import { GetStaticPaths, GetStaticProps } from 'next';
 import React from 'react';
+import Styles from '../../styles/Home.module.css';
+import Head from 'next/head';
 
-const Post = () => {
-  return <div>Post</div>;
+const Post = ({
+  postData,
+}: {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
+}) => {
+  return (
+    <div>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={Styles.headingXl}>{postData.title}</h1>
+        <div>{postData.date}</div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </div>
+  );
 };
 
 export default Post;
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id as string);
+  return {
+    props: {
+      postData,
+    },
+  };
+};
